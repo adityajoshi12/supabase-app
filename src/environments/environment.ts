6cb07091d7eae78afe230f549c195f3b594dd88a
aditya <adityaprakashjoshi1@gmail.com>
2021-09-04 19:48:57 +0530

    login added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: "https://hkzifgsimevlfhlfzxyy.supabase.co",
-  supbaseKey : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU1OTExNiwiZXhwIjoxOTQ2MTM1MTE2fQ.TSqWsZoEqJXnJLMjjbi42I79atYNrcukh-uThjV9GIs'
+  supabaseUrl: "",
+  supbaseKey: ''
 };
